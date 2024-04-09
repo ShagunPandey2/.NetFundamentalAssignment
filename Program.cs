279206f886c07a7dc7cb1f048f0f1c9853f3e22e
ShagunPandey2 <shagun.pandey@accolitedigital.com>
2024-04-09 15:33:00 +0530

    intial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int c = Convert.ToInt32(a);
+            string b=Console.ReadLine();
+            int d = Convert.ToInt32(b);
+            Console.WriteLine(a);   
+            Console.WriteLine(b);
+            Console.WriteLine(c+d);
+            Console.ReadLine();
+         
+        }
+    }
+}
